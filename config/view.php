154e0e9930d96fb0dc2061bd1d0f1c76c9c2697d
Sarah Withee <github@geekygirlsarah.com>
2018-06-06 00:41:57 -0400

    Split welcome.blade.php into template + content

--- config/view.php
+++ config/view.php
@@ -7,7 +7,7 @@ return [
     | View Storage Paths
     |--------------------------------------------------------------------------
     |
-    | Most templating systems load templates from disk. Here you may specify
+    | Most templating systems load layouts from disk. Here you may specify
     | an array of paths that should be checked for your views. Of course
     | the usual Laravel view path has already been registered for you.
     |
@@ -22,7 +22,7 @@ return [
     | Compiled View Path
     |--------------------------------------------------------------------------
     |
-    | This option determines where all the compiled Blade templates will be
+    | This option determines where all the compiled Blade layouts will be
     | stored for your application. Typically, this is within the storage
     | directory. However, as usual, you are free to change this value.
     |
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,39 +1,8 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="description" content="CodeThesaurus: A polyglot developer reference tool" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@extends('layouts.main')
 
-        <title>Code Thesaurus</title>
+@section('title', 'Welcome')
 
-        <!-- Bootstrap -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" />
-
-        <!-- Fonts -->
-        <!-- <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css"> -->
-
-        <!-- Styles -->
-        <link rel="stylesheet" href="css/app.css" />
-    </head>
-    <body>
-    <header>
-        <div class="navbar navbar-dark bg-blue box-shadow">
-            <div class="container d-flex justify-content-between">
-                <a href="#" class="navbar-brand d-flex align-items-center">
-                    <strong>Code Thesaurus</strong>
-                </a>
-                {{--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">--}}
-                    {{--<span class="navbar-toggler-icon"></span>--}}
-                {{--</button>--}}
-            </div>
-        </div>
-    </header>
-
-    <main role="main">
-
+@section('content')
         <section class="jumbotron text-center">
             <div class="container">
                 <h1 class="jumbotron-heading">The Polyglot Developer Reference</h1>
@@ -151,27 +120,4 @@
                 </div>
             </div>
         </div>
-
-    </main>
-
-
-    <footer class="text-muted">
-        <div class="container">
-            <p class="float-right">
-                <a href="#">Back to top</a>
-            </p>
-            <p>Made with &#x2764; by Sarah Withee.</p>
-            <p>Want to help out? Check the project out on <a href="http://github.com/codethesaurus/" target="_blank">GitHub</a>.</p>
-        </div>
-    </footer>
-
-    <!--scripts loaded here-->
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-
-    <script src="js/app.js"></script>
-
-    </body>
-</html>
+@endsection
