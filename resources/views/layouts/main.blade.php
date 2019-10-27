817d55311ce4e0c7050cde81d951c78acbaa8a09
Sarah Withee <github@geekygirlsarah.com>
2019-10-27 01:15:10 -0400

    Add About page (view, controller) and fix navigation menu to show page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,25 +16,58 @@
     <!-- <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css"> -->
 
     <!-- Styles -->
-    <link rel="stylesheet" href="css/app.css" />
+    <link rel="stylesheet" href="/css/app.css" />
 </head>
 <body>
 <header>
-    <div class="navbar navbar-dark bg-blue box-shadow">
+    <nav class="navbar navbar-dark navbar-expand-sm bg-blue box-shadow">
         <div class="container d-flex justify-content-between">
-            <a href="/" class="navbar-brand d-flex align-items-center">
-                <strong>Code Thesaurus</strong>
-            </a>
-            {{-- Add this all back in if the hamburger menu is needed}}
-            {{--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">--}}
-            {{--<span class="navbar-toggler-icon"></span>--}}
-            {{--</button>--}}
+            {{-- Title --}}
+            <a class="navbar-brand d-flex align-items-center" href="/">Code Thesaurus</a>
+
+            {{-- Hamburger Menu --}}
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
         </div>
-    </div>
+
+        <div class="collapse navbar-collapse" id="navbarToggler">
+            {{-- Navigation Menu --}}
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item mr-auto">               {{-- add this into class of <li>: active --}}
+                    <a class="nav-link" href="/">Home</a>   {{-- add this to inside of <a>:   <span class="sr-only">(current)</span> --}}
+                </li>
+                <li class="nav-item mr-auto">
+                    <a class="nav-link" href="/about/">About</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+
+{{--    --}}
+{{--    <div class="navbar navbar-dark bg-blue box-shadow">--}}
+{{--        <div class="container d-flex justify-content-between">--}}
+{{--            <a href="/" class="navbar-brand d-flex align-items-center">--}}
+{{--                <strong>Code Thesaurus</strong>--}}
+{{--            </a>--}}
+
+{{--            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">--}}
+{{--                <span class="navbar-toggler-icon"></span>--}}
+{{--            </button>--}}
+
+{{--            <div class="collapse navbar-collapse" id="navbarSupportedContent">--}}
+{{--                <div class="navbar-nav">--}}
+{{--                    <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>--}}
+{{--                    <a class="nav-item nav-link" href="/about/">About</a>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
 </header>
 
 <main role="main">
-    @yield('content')
+@yield('content')
 </main>
 
 
@@ -54,7 +87,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 
-<script src="js/app.js"></script>
+<script src="/js/app.js"></script>
 
 </body>
 </html>
