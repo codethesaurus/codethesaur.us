a57a59eed91dbc6e1b26f848206e79ec496b9475
Sarah Withee <gitlab@geekygirlsarah.com>
2020-09-27 22:14:35 -0400

    Remove that pesky secret key (that, thankfully, wasn't used anywhere in prod)

--- codethesaurus/settings.py
+++ codethesaurus/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^m6j#o46lnh*@7x#=jo003pzp$9pk77)!p1rblv!26ma=5ye$q'
+SECRET_KEY = os.environ.get("SECRET_KEY", "default-unsafe-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
