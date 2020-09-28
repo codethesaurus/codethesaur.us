123aaabc18e72e405b76884bcad3063f93db43cb
Sarah Withee <gitlab@geekygirlsarah.com>
2020-09-28 19:47:03 -0400

    Add control/view and template code for Compare page

--- web/views.py
+++ web/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+import json
+
 
 # from .models import Question
 
@@ -8,37 +10,76 @@ def index(request):
     content = {
         'title': 'Welcome'
     }
-    return render(request, 'web/index.html', content)
+    return render(request, 'index.html', content)
+
 
 def about(request):
     content = {
         'title': 'About'
     }
-    return render(request, 'web/about.html', content)
+    return render(request, 'about.html', content)
 
-# def detail(request, question_id):
-#     # question = get_object_or_404(Question, pk=question_id)
-#     render(request, 'web/detail.html', {'hi': 'things'})
-#
-# def results(request, question_id):
-#     # question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/results.html', {'question': 'question'})
-#
-# def vote(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     try:
-#         selected_choice = question.choice_set.get(pk=request.POST['choice'])
-#     except (KeyError, Choice.DoesNotExist):
-#         # Redisplay the question voting form.
-#         return render(request, 'polls/detail.html', {
-#             'question': question,
-#             'error_message': "You didn't select a choice.",
-#         })
-#     else:
-#         selected_choice.votes += 1
-#         selected_choice.save()
-#         # Always return an HttpResponseRedirect after successfully dealing
-#         # with POST data. This prevents data from being posted twice if a
-#         # user hits the Back button.
-#         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-#
+
+def compare(request):
+    #JSON updates:
+    # language => id
+    # language_name => friendly_label
+
+    # TODO: try/catch
+
+    with open("web/thesauruses/meta_info.json", 'r') as meta_file:
+        meta_data = meta_file.read()
+    meta_data_langs = json.loads(meta_data)["languages"]
+
+    lang1 = request.GET.get('lang1', '')
+    lang2 = request.GET.get('lang2', '')
+    lang1_directory = meta_data_langs[lang1]
+    lang2_directory = meta_data_langs[lang2]
+
+    concept = request.GET.get('concept', '')
+
+    with open("web/thesauruses/" + lang1_directory + "/" + concept + ".json", 'r') as lang1_file:
+        data = lang1_file.read()
+    # parse file
+    lang1_file_json = json.loads(data)
+    lang1_concept = lang1_file_json[concept]
+    lang1_friendlyname = lang1_file_json["meta"]["language_name"]
+
+    with open("web/thesauruses/" + lang2_directory + "/" + concept + ".json", 'r') as lang2_file:
+        data = lang2_file.read()
+    # parse file
+    lang2_file_json = json.loads(data)
+    lang2_concept = lang2_file_json[concept]
+    lang2_friendlyname = lang2_file_json["meta"]["language_name"]
+
+    common_concepts =[]
+    for key in list(set.union(set(lang1_concept.keys()), set(lang2_concept.keys()))):
+        common_concepts.append({
+            "key": key,
+            "lang1": lang1_concept[key],
+            "lang2": lang2_concept[key]
+        })
+
+
+    # DB equivalent of full outer join
+    response = {
+        "title": "Comparing" + lang1 + " " + lang2,
+        "concept": concept,
+        "lang1": lang1,
+        "lang2": lang2,
+        "lang1_friendlyname": lang1_friendlyname,
+        "lang2_friendlyname": lang2_friendlyname,
+        # "categories": categories
+        #"lang1Concepts": lang1_concept,   #remove?
+        #"lang2Concepts": lang2_concept    #remove?
+        "common_concepts": common_concepts
+    }
+
+    return render(request, 'compare.html', response)
+
+# add in compare with /compare/lang1/lang2 in the future
+
+
+def reference(request, language):
+    return HttpResponse("Placeholder...")
+
