cb8d3575fb7853406669d274176a7dee8d2f7243
Sarah Withee <gitlab@geekygirlsarah.com>
2021-04-07 15:00:22 -0400

    Rename reference get to valid language, add reference invalid language test

--- web/tests/test_views.py
+++ web/tests/test_views.py
@@ -5,7 +5,6 @@ from web.views import index, about, compare, reference
 
 class TestViews(TestCase):
 
-
 	def test_index_view_GET(self):
 		url = reverse('index')
 		response = self.client.get(url)
@@ -13,7 +12,6 @@ class TestViews(TestCase):
 		self.assertEqual(response.status_code, 200)
 		self.assertTemplateUsed(response, 'index.html')
 		self.assertTemplateUsed(response, 'base.html')
-
 
 	def test_about_view_GET(self):
 		url = reverse('about')
@@ -23,7 +21,6 @@ class TestViews(TestCase):
 		self.assertTemplateUsed(response, 'about.html')
 		self.assertTemplateUsed(response, 'base.html')
 
-
 	def test_compare_view_both_valid_languages(self):
 		url = reverse('compare') + '?concept=data_types&lang1=python&lang2=java'
 		response = self.client.get(url)
@@ -31,7 +28,6 @@ class TestViews(TestCase):
 		self.assertEquals(response.status_code, 200)
 		self.assertTemplateUsed(response, 'compare.html')
 		self.assertTemplateUsed(response, 'base.html')
-
 
 	def test_compare_view_invalid_languages(self):
 		url = reverse('compare') + '?concept=data_types&lang1=cupcake&lang2=donut'
@@ -49,11 +45,19 @@ class TestViews(TestCase):
 		self.assertTemplateNotUsed(response, 'compare.html')
 		self.assertTemplateNotUsed(response, 'base.html')
 
-
-	def test_reference_view_GET(self):
+	def test_reference_view_valid_language(self):
 		url = reverse('reference') + '?concept=data_types&lang=python'
 		response = self.client.get(url)
 
 		self.assertEquals(response.status_code, 200)
 		self.assertTemplateUsed(response, 'reference.html')
 		self.assertTemplateUsed(response, 'base.html')
+
+	def test_reference_view_invalid_languages(self):
+		url = reverse('reference') + '?concept=data_types&lang=cupcake'
+		response = self.client.get(url)
+
+		self.assertEquals(response.status_code, 404)
+		self.assertTemplateNotUsed(response, 'reference.html')
+		self.assertTemplateNotUsed(response, 'base.html')
+
