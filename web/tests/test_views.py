dc064dc3e8ed9e4ee078ac6fdd374523cb4e95dc
Sarah Withee <gitlab@geekygirlsarah.com>
2021-04-07 14:51:49 -0400

    Add test for testing invalid languages on compare

--- web/tests/test_views.py
+++ web/tests/test_views.py
@@ -33,6 +33,15 @@ class TestViews(TestCase):
 		self.assertTemplateUsed(response, 'base.html')
 
 
+	def test_compare_view_invalid_languages(self):
+		url = reverse('compare') + '?concept=data_types&lang1=cupcake&lang2=donut'
+		response = self.client.get(url)
+
+		self.assertEquals(response.status_code, 404)
+		self.assertTemplateNotUsed(response, 'compare.html')
+		self.assertTemplateNotUsed(response, 'base.html')
+
+
 	def test_reference_view_GET(self):
 		url = reverse('reference') + '?concept=data_types&lang=python'
 		response = self.client.get(url)
