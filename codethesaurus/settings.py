1ff3cf8ec350d42c984efa935afbe38da14ecb36
Sarah Withee <gitlab@geekygirlsarah.com>
2021-04-08 19:03:29 -0400

    Reenable Sqlite but keep test runners disabled

--- codethesaurus/settings.py
+++ codethesaurus/settings.py
@@ -84,13 +84,12 @@ WSGI_APPLICATION = 'codethesaurus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
