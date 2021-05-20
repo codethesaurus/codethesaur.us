3b866f55c4848b7fce5d05a35111a0a30bf3e285
Sarah Withee <gitlab@geekygirlsarah.com>
2021-05-20 01:29:36 -0400

    Fix STATICFILES_DIRS to where static files are, not where they're going and ensure dir exists

--- codethesaurus/settings.py
+++ codethesaurus/settings.py
@@ -136,15 +136,17 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-os.makedirs('STATIC_ROOT', exist_ok=True)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+os.makedirs(STATIC_ROOT, exist_ok=True)
+# os.makedirs(os.path.join(STATIC_ROOT, 'images'), exist_ok=True)
+# os.makedirs(os.path.join(STATIC_ROOT, 'css'), exist_ok=True)
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, 'static'),
-    'static/images',
-    'static/css',
+    os.path.join(BASE_DIR, 'static'),
+    # 'static/images',
+    # 'static/css',
     # 'staticfiles'
 ]
 
