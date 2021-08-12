a54039eaf7d907235a560d2baeb7d51ac5fb324f
Sarah Withee <gitlab@geekygirlsarah.com>
2021-08-12 13:02:00 -0400

    Update request to get first 100 contributors including anonymous ones

--- web/static/js/contributors.js
+++ web/static/js/contributors.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  jQuery.get('https://api.github.com/repos/codethesaurus/codethesaur.us/contributors', function(data, status){
+  jQuery.get('https://api.github.com/repos/codethesaurus/codethesaur.us/contributors?accept=application/vnd.github.v3+json&per_page=100&anon=1', function(data, status){
     $("#contributors").text(data.length)
   })
 })
