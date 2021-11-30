bed2c303b5216a82a609825b044f16fd6729c41b
oneamanish <manishone0101@gmail.com>
2021-11-30 11:42:29 -0800

    AddedString

---
+++ string.java
@@ -0,0 +1,8 @@
+public class StringDemo {
+
+    public static void main(String args[]) {
+       char[] helloArray = { 'h', 'e', 'l', 'l', 'o', '.' };
+       String helloString = new String(helloArray);  
+       System.out.println( helloString );
+    }
+ }
