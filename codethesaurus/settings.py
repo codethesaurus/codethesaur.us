d1731976bc008811036e948612090705addbd70f
Sarah Withee <2601974+geekygirlsarah@users.noreply.github.com>
2022-03-07 01:05:24 -0500

    Add override for Heroku's database connection settings

--- codethesaurus/settings.py
+++ codethesaurus/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 import django_on_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -94,6 +96,9 @@ DATABASES = {
         'DEFAULT_AUTO_FIELD': 'django.db.models.AutoField'
     }
 }
+# Heroku: Update database configuration from $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
