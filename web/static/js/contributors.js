465e9c0442a2423ef01845cb57b484a6e0a9d25c
splashley <ashleyjlhynes@gmail.com>
2022-04-14 16:27:58 -0400

    add handle to support 100+ contributors on footer

--- web/static/js/contributors.js
+++ web/static/js/contributors.js
@@ -1,5 +1,22 @@
-$(document).ready(function(){
-  jQuery.get('https://api.github.com/repos/codethesaurus/codethesaur.us/contributors?accept=application/vnd.github.v3+json&per_page=100&anon=1', function(data, status){
-    $("#contributors").text(data.length)
-  })
-})
+$(document).ready(function () {
+  let i = 1;
+  let length = 0;
+  const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+  const timer = async () => {
+    await sleep(1000);
+    jQuery
+      .get(
+        `https://api.github.com/repos/codethesaurus/codethesaur.us/contributors?accept=application/vnd.github.v3+json&per_page=100&page=${i}&anon=1`
+      )
+      .then((data) => {
+        length += data.length;
+        i++;
+        if (data.length === 0) {
+          $("#contributors").text(length);
+        } else {
+          timer();
+        }
+      });
+  };
+  timer();
+});
