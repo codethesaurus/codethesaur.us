5abdd4f255503123a5bc28b3e5e08cf56e4239a9
Shreya <a.shreya202@gmail.com>
2022-10-22 21:34:40 +0530

    Revert settings.py change

--- codethesaurus/settings.py
+++ codethesaurus/settings.py
@@ -97,8 +97,8 @@ DATABASES = {
     }
 }
 # Heroku: Update database configuration from $DATABASE_URL.
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
