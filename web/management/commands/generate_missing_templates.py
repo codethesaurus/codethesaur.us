0f0ae0be4b1bdf34d42aad0093a0aa6315532245
Ulysse ARNAUD <mexanga@gmail.com>
2022-10-23 15:52:45 +0200

    Command generate_missing_templates to generate missing thesaurus with draft mode

---
+++ web/management/commands/generate_missing_templates.py
@@ -0,0 +1,31 @@
+from django.core.management.base import BaseCommand
+
+from web.models import Language, MetaInfo
+
+import os
+
+from web.thesaurus_template_generators import generate_language_template
+
+
+class Command(BaseCommand):
+    help = 'Generate language thesaurus files to be filled out'
+
+    def handle(self, *args, **options):
+        # retrieve all languages and structures
+        meta_info = MetaInfo()
+        languages = meta_info.languages
+        structures = meta_info.structures
+        
+        for language in languages:
+            versions = Language(language, languages[language]).versions()
+            for version in versions:
+                for structure in structures:
+                    file_path = os.path.join(
+                        'web',
+                        'thesauruses',
+                        language,
+                        version,
+                        structure + '.json'
+                    )
+                    if not os.path.exists(file_path):
+                        os.system(f'python manage.py generate_template {language} {structure} --language-version {version} --draft=True')
