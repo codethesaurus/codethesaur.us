79ccd529d88a9427b01e319b9c8fd82403bdb28c
tal66 <77445020+tal66@users.noreply.github.com>
2023-11-20 21:08:09 +0000

    rows

--- web/templatetags/templatetags.py
+++ web/templatetags/templatetags.py
@@ -6,7 +6,10 @@ register = template.Library()
 @register.inclusion_tag('concept_card.html')
 def concept_card(code, comment):
     """tag for a single concept"""
+    if isinstance(comment, list):
+        comment = "\n".join(comment)
+
     return {
         'code': code,
-        'comment': str(comment)
+        'comment': comment
     }
